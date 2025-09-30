df0423b6cc579bbfe1cf6efd52d07387ab105925
Bilal Arsal <bilalarsal24@gmail.com>
2025-09-30 13:54:31 +0500

    Fix saveProfile: safe defaults, moved profileData to functions, public image path

--- netlify/functions/saveProfile.js
+++ netlify/functions/saveProfile.js
@@ -31,7 +31,7 @@ export async function handler(event, context) {
       RETURNING *
       `,
       [
-        ProfileData?.name || "Dummy",
+        ProfileData?.name || "Dummyy",
         ProfileData?.records?.winStreak || 0,
         ProfileData?.records?.trophies || 0,
         ProfileData?.records?.victories|| 0,
